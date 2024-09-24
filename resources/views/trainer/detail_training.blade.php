50340d178c3df5ba10abb07885c8060bdcb83304
Rifqi Irfansyah <rifqiirfansyah518@gmail.com>
2024-09-24 23:08:24 +0700

    add popup to add meet st

--- resources/views/trainer/detail_training.blade.php
+++ resources/views/trainer/detail_training.blade.php
@@ -32,7 +32,7 @@
             <?php $i++; ?>
             @endforeach
 
-            <li class="d-flex align-items-center ps-4 py-1 mb-3">
+            <li class="d-flex align-items-center ps-4 py-1 mb-3" onClick="buttonEdit()">
                 <a href="#" class=" text-decoration-none">
                     <span class="fs-6 fw-bold my-1">Add Meet {{$i}}st</span>
                 </a>
@@ -54,7 +54,7 @@
                     <h1>{{$training->judul_training}}</h1>
                 </div>
                 <div class="col-2 d-flex justify-content-end align-items-center">
-                    <button class="btn btn-md fs-6 rounded-5 btn-custom w-100 py-2">
+                    <button class="btn btn-md fs-6 rounded-5 btn-custom w-100 py-2" onClick="buttonEdit()">
                         <i class="fa-regular fa-user"></i> Edit Training
                     </button>
                 </div>
@@ -117,4 +117,53 @@ toggle.onclick = function() {
     navigation.classList.toggle("active");
     main.classList.toggle("active");
 };
+</script>
+
+<script>
+    function buttonEdit() {
+    var title = "Add " + "<?php echo $i; ?>" + "st Meet\n" + "<?php echo $training->judul_training; ?>" + "\n\n";
+    (async () => {
+        const {
+            value: formValues
+        } = await Swal.fire({
+            title: title,
+            html: `
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class ="col-4">Start Meet</div>
+                    <div class ="col-8 d-flex align-items-center">
+                        <input id="swal-input1" class="form-control form-control-lg bg-light fs-6 rounded-5 ps-4">
+                    </div>
+                </div>
+
+                <div class="row align-items-center mt-2">
+                    <div class ="col-4">End Meet</div>
+                    <div class ="col-8 d-flex align-items-center">
+                        <input id="swal-input2" class="form-control form-control-lg bg-light fs-6 rounded-5 ps-4">
+                    </div>
+                </div>
+
+                
+            </div>
+        `,
+            focusConfirm: false,
+
+            preConfirm: () => {
+                return [
+                    document.getElementById("swal-input1").value,
+                    document.getElementById("swal-input2").value
+                ];
+            },
+            customClass: {
+                popup: 'popup-edit',
+                confirmButton: 'btn-confirm',
+                title: 'title',
+                color: '#DE2323',
+            }
+        });
+        if (formValues) {
+            Swal.fire(JSON.stringify(formValues));
+        }
+    })()
+}
 </script>
